274183b91c54b80e9365fa2bbc46ae342ef12e24
Estevão Rodrigues <estevao.rodrigues@optsol.com.br>
2022-10-06 11:10:30 -0300

    Criação do layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'meetup';
+
+  listData: IUserModel[] = [];
+
+  cadastroForm: FormGroup;
+
+  constructor(
+    private fb: FormBuilder
+  ) { }
+
+  ngOnInit(): void {
+    this.cadastroForm = this.fb.group({
+      email: [null],
+      senha: [null]
+    });
+  }
+
+  btnSalvar(){
+    debugger;
+    console.log('salvar')
+  }
+
+  btnEsqueciSenha(){
+    console.log('esqueci senha')
+  }
+}
+
+export interface IUserModel {
+  nome: string;
+  senha: string;
+  data: Date;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,41 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { pt_BR } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import pt from '@angular/common/locales/pt';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzTableModule } from 'ng-zorro-antd/table';
+
+const nzMods = [
+  NzFormModule,
+  NzInputModule,
+  NzButtonModule,
+  NzTableModule
+];
+
+registerLocaleData(pt);
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    ...nzMods
   ],
-  providers: [],
+  providers: [{ provide: NZ_I18N, useValue: pt_BR }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
